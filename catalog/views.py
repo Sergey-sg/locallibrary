78c53df8ca5a037e75ca513117227f45f54b03fc
Sergey Glushak <glushak_sergey@i.ua>
2021-11-09 00:30:38 +0200

    создание аутентификации пользователей

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def index(request):
@@ -16,8 +17,11 @@ def index(request):
     num_genre = Genre.objects.all().count()
     num_books_with_words_in_title = Book.objects.filter(title__iregex=r'\w+').count()
 
-    # Отрисовка HTML-шаблона index.html с данными внутри
-    # переменной контекста context
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    # Render the HTML template index.html with the data in the context variable.
     return render(
         request,
         'index.html',
@@ -26,8 +30,7 @@ def index(request):
             'num_instances': num_instances,
             'num_instances_available': num_instances_available,
             'num_authors': num_authors,
-            'num_genre': num_genre,
-            'num_books_with_words_in_title': num_books_with_words_in_title,
+            'num_visits': num_visits,    # num_visits appended
         },
     )
 
@@ -49,3 +52,26 @@ class AuthorListView(generic.ListView):
 class AuthorDetailView(generic.DetailView):
     model = Author
 
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """
+    Generic class-based view listing books on loan to current user.
+    """
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+
+class LoanedBooksByLibrarianListView(LoginRequiredMixin, generic.ListView):
+    """
+    Generic class-based view listing books on loan to current librarian.
+    """
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_librarian.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
