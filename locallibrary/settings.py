0c2607b53ff1c44f3b55162d023e81fbf1f18cd2
Sergey Glushak <glushak_sergey@i.ua>
2021-11-13 21:06:50 +0200

    Added files and changes required for deployment to heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,11 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['locallibrary.herokuapp.com',
-                 '127.0.0.1',
-                 'https://frozen-shore-46711.herokuapp.com/',
-                 'frozen-shore-46711.herokuapp.com',
-                 ]
+ALLOWED_HOSTS = ['frozen-shore-46711.herokuapp.com', '127.0.0.1']
 
 # For example:
 # ALLOWED_HOSTS = [
